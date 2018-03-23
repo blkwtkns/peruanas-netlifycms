cf9b11fb432e11c5e6c0c16ef1c84c5314dc1730
Blake Watkins <iamsoblake@gmail.com>
2018-03-22 23:02:31 -0700

    update data

--- app/src/components/Home/Home.js
+++ app/src/components/Home/Home.js
@@ -3,18 +3,18 @@ import About from './About'
 // import { images } from '../../constants/photoState';
 // import ImageCarousel from '../ImageCarousel';
 import Intro from './Intro'
-import Separator from '../SepComponent'
-import Gallery from '../LightGallery';
+// import Separator from '../SepComponent'
+// import Gallery from '../LightGallery';
 import { withRouteData } from 'react-static'
 
 // <ImageCarousel images={ images }/>
 // <Separator clName={'aboutSeparator'} />
 
-// export default props => (
-export default withRouteData(({images}) => (
+export default props => (
+// export default withRouteData(({images}) => (
   <section className="homeSection">
     <Intro />
-    <Gallery clName={'HomeGallery'} images={images} />
+    {/* <Gallery clName={'HomeGallery'} images={images} /> */}
     <About />
   </section>
-))
+)
--- app/static.config.js
+++ app/static.config.js
@@ -54,8 +54,8 @@ export default {
   }),
   getRoutes: async () => {
     const presentations = await getPosts('presentations')
-    const logos = await getPosts('logos')
-    const images = await getPosts('home')
+    // const logos = await getPosts('logos')
+    // const images = await getPosts('home')
     // const dancers = await getPosts('dancers')
     // const dances = await getPosts('dances')
     // const photos = await getPosts('photos')
@@ -64,10 +64,10 @@ export default {
       {
         path: '/',
         component: 'src/components/Home/Home',
-        getData: () => ({
-          logos,
-          images,
-        }),
+        // getData: () => ({
+        //   logos,
+        //   images,
+        // }),
       },
       {
         path: '/history',
