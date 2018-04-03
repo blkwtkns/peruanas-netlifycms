3189d59ef3b2cdd2aea21d98c2c5a74caf13e5b4
Blake Watkins <iamsoblake@gmail.com>
2018-04-02 22:03:08 -0700

    style tweaks

--- src/components/Home/Intro.js
+++ src/components/Home/Intro.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default () => (
   <div className="Intro">
     <div className="greyBox">
-      <h1 className="intro-title">Peruvian Dance, Music, and Culture</h1>
+      <h1 className="intro-title">Raices Peruanas</h1>
+      <h1 className="intro-subtitle">Dance, Music, Culture</h1>
     </div>
   </div>
 )
--- src/components/Main/RouteLinks.js
+++ src/components/Main/RouteLinks.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import { Link } from 'react-static'
 
-// <nav className="nav-right">
-// <li><Link to="/"><span className="logo">Raices Peruanas</span></Link></li>
 export default ({ logo }) => (
   <nav className="nav">
     <ul className="nav__routes" role="navigation">
+      <li><Link to="/"><img src={logo} className={'raicesLogo'} alt="peruanos logo" /></Link></li>
       <li><Link to="/about">About</Link></li>
       <li><Link to="/presentations">Presentations</Link></li>
       <li><Link to="/dances">Dances</Link></li>
-      <li><Link to="/"><img src={logo} className={'raicesLogo'} alt="peruanos logo" /></Link></li>
       <li><Link to="/history">History</Link></li>
       <li><Link to="/contact">Contact</Link></li>
       <li><Link to="/dancers">Dancers</Link></li>
