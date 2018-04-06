77faa70754c3191d4c26c408798ab86b32759d5b
Blake Watkins <iamsoblake@gmail.com>
2018-04-05 21:11:30 -0700

    Fix conditional

--- src/components/Main/Footer.js
+++ src/components/Main/Footer.js
@@ -16,28 +16,33 @@ class Footer extends Component {
   }
 
   render () {
-    if (this.props.visible) return ( <Footie /> )
-    return ( <div></div> )
+    return (
+      <div>
+        <Footie visible={this.props.visible} />
+      </div>
+    )
   }
 }
 
-const Footie = () => (
-  <footer className="footer">
-    <div className="containter">
-      <Sociolinks />
-      <p>RaicesPeruanas.com © Copyright 2004 - 2013</p>
-      <p>
-        <em>
-          Peruvian Events in Los Angeles. Promoting music and traditional dances
-          from Perú
-        </em>
-      </p>
-      <p>
-        Web page created, updated and Maintained by:{' '}
-        <a href="blkmwtkns.co">Blake Watkins</a>
-      </p>
-    </div>
-  </footer>
-)
+const Footie = ({ visible }) => {
+  return !visible ? null : (
+    <footer className="footer">
+      <div className="containter">
+        <Sociolinks />
+        <p>RaicesPeruanas.com © Copyright 2004 - 2013</p>
+        <p>
+          <em>
+            Peruvian Events in Los Angeles. Promoting music and traditional dances
+            from Perú
+          </em>
+        </p>
+        <p>
+          Web page created, updated and Maintained by:{' '}
+          <a href="blkmwtkns.co">Blake Watkins</a>
+        </p>
+      </div>
+    </footer>
+  )
+}
 
 export default connect(mapStateToProps)(Footer)
