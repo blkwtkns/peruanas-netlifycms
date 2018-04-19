f9a45037f478eabc4734e2a17060f69467a7603e
Blake Watkins <iamsoblake@gmail.com>
2018-04-18 21:26:59 -0700

    CSS changes for dances and dancers

--- src/components/Dancers/Dancers.js
+++ src/components/Dancers/Dancers.js
@@ -1,40 +1,39 @@
 import React from 'react'
 import { withRouteData, Link } from 'react-static'
 
-export default withRouteData(({ dancers }) => (
-  <section className="mainSection">
-    <div className="dancers__container">
-      <h1 className="dancers--title">Dancers of Raices Peruanas</h1>
-      <br />
-
-      {dancers.map((dancer, idx) => {
-        if (idx !== 0) {
-          return null
-        }
-        return (
-          <div className="dancer--featured" key={dancer.data.slug}>
-            <Link to={`/dancers/dancer/${dancer.data.slug}`}>
-              <img src={dancer.data.thumbnail} alt={dancer.data.title} />
-            </Link>
-          </div>
-        )
-      })}
-      <div className="dancers-grid">
-        {dancers.map((dancer, idx) => {
-          if (idx === 0) {
-            return null
-          }
-          return (
-            <div className="dancer-col" key={dancer.data.slug}>
-              <Link to={`/dancers/dancer/${dancer.data.slug}`}>
-                <img src={dancer.data.thumbnail} alt={dancer.data.title} />
+export default withRouteData(({ dancers }) => {
+  const featuredIdx = Math.floor(Math.random() * dancers.length);
+  return (
+    <section className="mainSection">
+      <div className="dancers__container">
+        <h1 className="dancers--title">Dancers of Raices Peruanas</h1>
+        <br />
+        {
+          <div className="dancer-contain">
+            <div className="dancer--featured" key={dancers[featuredIdx].data.slug}>
+              <Link to={`/dancers/dancer/${dancers[featuredIdx].data.slug}`}>
+                <img src={dancers[featuredIdx].data.thumbnail} alt={dancers[featuredIdx].data.title} />
               </Link>
             </div>
-          )
-        })}
+          </div>
+        }
+        <div className="dancers-grid">
+          {dancers.map((dancer, idx) => {
+            if (idx === featuredIdx) {
+              return null
+            }
+            return (
+              <div className="dancer-contain">
+                <div className="dancer-col" key={dancer.data.slug}>
+                  <Link to={`/dancers/dancer/${dancer.data.slug}`}>
+                    <img src={dancer.data.thumbnail} alt={dancer.data.title} />
+                  </Link>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
-    </div>
-  </section>
-))
-
-
+    </section>
+  )
+})
--- src/components/Dances/Dances.js
+++ src/components/Dances/Dances.js
@@ -1,37 +1,39 @@
 import React from 'react'
 import { withRouteData, Link } from 'react-static'
 
-export default withRouteData(({ dances }) => (
-  <section className="mainSection">
-    <div className="dances-container">
-      <h1 className="dances--title">Dances of Peru</h1>
-      <br />
-      {dances.map((dance, idx) => {
-        if (idx !== 0) {
-          return null
-        }
-        return (
-          <div className="dances--featured" key={dance.data.slug}>
-            <Link to={`/dances/dance/${dance.data.slug}`}>
-              <img src={dance.data.thumbnail} alt={dance.data.title} />
-            </Link>
-          </div>
-        )
-      })}
-      <div className="dances-grid">
-        {dances.map((dance, idx) => {
-          if (idx === 0) {
-            return null
-          }
-          return (
-            <div className="dances-col" key={dance.data.slug}>
-              <Link to={`/dances/dance/${dance.data.slug}`}>
-                <img src={dance.data.thumbnail} alt={dance.data.title} />
+export default withRouteData(({ dances }) => {
+  const featuredIdx = Math.floor(Math.random() * dances.length);
+  return (
+    <section className="mainSection">
+      <div className="dances-container">
+        <h1 className="dances--title">Dances of Peru</h1>
+        <br />
+        {
+          <div className="dances-contain">
+            <div className="dances--featured" key={dances[featuredIdx].data.slug}>
+              <Link to={`/dances/dance/${dances[featuredIdx].data.slug}`}>
+                <img src={dances[featuredIdx].data.thumbnail} alt={dances[featuredIdx].data.title} />
               </Link>
             </div>
-          )
-        })}
+          </div>
+        }
+        <div className="dances-grid">
+          {dances.map((dance, idx) => {
+            if (idx === featuredIdx) {
+              return null
+            }
+            return (
+              <div className="dances-contain">
+                <div className="dances-col" key={dance.data.slug}>
+                  <Link to={`/dances/dance/${dance.data.slug}`}>
+                    <img src={dance.data.thumbnail} alt={dance.data.title} />
+                  </Link>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
-    </div>
-  </section>
-))
+    </section>
+  )
+})
