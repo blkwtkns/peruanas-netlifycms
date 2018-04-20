0562fbea5f17ce93e71a3973d452beed7ff06542
Blake Watkins <iamsoblake@gmail.com>
2018-04-20 00:27:47 -0700

    Fix nav. Add more mobile styles.

--- src/components/Main/RouteLinks.js
+++ src/components/Main/RouteLinks.js
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-static'
 
-export default ({ logo, stasis }) => {
-  let linkVisibility = stasis ? 'linksVisible' : 'linksHidden'
+export default class Routelinks extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      listVisibility: true
+    };
+    this.toggleMobileMenu = this.toggleMobileMenu.bind(this)
+  }
 
-  return (
-    <nav className="nav">
-      <ul className="nav__routes" role="navigation">
-        <li className="raicesLink"><Link to="/"><img src={logo} className={'raicesLogo'} alt="peruanos logo" /></Link></li>
-        <li className={ linkVisibility }><Link to="/about">About</Link></li>
-        <li className={ linkVisibility }><Link to="/presentations">Presentations</Link></li>
-        <li className={ linkVisibility }><Link to="/dances">Dances</Link></li>
-        <li className={ linkVisibility }><Link to="/history">History</Link></li>
-        <li className={ linkVisibility }><Link to="/contact">Contact</Link></li>
-        <li className={ linkVisibility }><Link to="/dancers">Dancers</Link></li>
-      </ul>
-    </nav>
-  )
+  toggleMobileMenu() {
+    this.setState({ listVisibility: !this.state.listVisibility });
+  }
+
+  toggleListClass() {
+    return this.state.listVisibility ? 'nav-ul hide-ul' : 'nav-ul';
+  }
+
+  toggleLinkVisibility(stasis) {
+    return stasis ? 'nav-link nav-links-home' : 'nav-link nav-links-else';
+  }
+
+  toggleHeaderBackground(stasis) {
+    return stasis ? 'nav-background nav-back-trans' : 'nav-background nav-back-white';
+  }
+
+  toggleMenuBackground(stasis) {
+    return stasis ? 'nav-menu-button nav-menu-btn-clr-home' : 'nav-menu-button nav-menu-btn-clr-else';
+  }
+
+  render() {
+    return (
+      <div className={this.toggleHeaderBackground(this.props.stasis)}>
+        <div id="nav-container">
+          <header id="nav-header">
+            <div className="raicesLink nav-logo"><Link to="/"><img src={this.props.logo} className='raicesLogo' alt="peruanos logo" /></Link></div>
+            <div className={this.toggleMenuBackground(this.props.stasis)} onClick={this.toggleMobileMenu}><i className="fas fa-bars fa-2x"></i></div>
+          </header>
+          <nav>
+            <ul className={this.toggleListClass()}>
+              <li className={this.toggleLinkVisibility(this.props.stasis)}><Link to="/about">About</Link></li>
+              <li className={this.toggleLinkVisibility(this.props.stasis)}><Link to="/presentations">Presentations</Link></li>
+              <li className={this.toggleLinkVisibility(this.props.stasis)}><Link to="/dances">Dances</Link></li>
+              <li className={this.toggleLinkVisibility(this.props.stasis)}><Link to="/history">History</Link></li>
+              <li className={this.toggleLinkVisibility(this.props.stasis)}><Link to="/dancers">Dancers</Link></li>
+              <li className={this.toggleLinkVisibility(this.props.stasis)}><Link to="/contact">Contact</Link></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    )
+  }
 }
